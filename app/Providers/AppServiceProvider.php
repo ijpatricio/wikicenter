feb60295f5e6addad34ed865467005cf8df6a76f
Joao Patricio <ijpatricio@gmail.com>
2023-03-02 20:59:49 +0000

    Installs GitPod environment.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Filament\Facades\Filament;
 use Illuminate\Foundation\Vite;
 use Illuminate\Support\ServiceProvider;
 use Z3d0X\FilamentFabricator\Facades\FilamentFabricator;
@@ -25,6 +26,13 @@ class AppServiceProvider extends ServiceProvider
         FilamentFabricator::registerStyles([
             app(Vite::class)('resources/css/app.css'),
         ]);
+
+        Filament::serving(function () {
+            // Using Vite
+            Filament::registerTheme(
+                app(Vite::class)('resources/css/app.css'),
+            );
+        });
 
         PageBuilder::configureUsing(function (PageBuilder $builder) {
             $builder->collapsed();
