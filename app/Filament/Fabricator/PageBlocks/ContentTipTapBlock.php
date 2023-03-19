0be049807d9291cecd8c329fe26a8757f28823e2
Amitav Roy <reachme@amitavroy.com>
2023-03-19 08:13:44 +0530

    #7 TOC for a page Added a toc field to content block And added a toc hash field so that we can generate unique toc has which will be used to create the anchor tag.

--- app/Filament/Fabricator/PageBlocks/ContentTipTapBlock.php
+++ app/Filament/Fabricator/PageBlocks/ContentTipTapBlock.php
@@ -2,8 +2,13 @@
 
 namespace App\Filament\Fabricator\PageBlocks;
 
+use Closure;
 use Filament\Forms\Components\Builder\Block;
+use Filament\Forms\Components\Fieldset;
+use Filament\Forms\Components\Hidden;
+use Filament\Forms\Components\TextInput;
 use FilamentTiptapEditor\TiptapEditor;
+use Illuminate\Support\Str;
 use Z3d0X\FilamentFabricator\PageBlocks\PageBlock;
 
 class ContentTipTapBlock extends PageBlock
@@ -12,6 +17,18 @@ class ContentTipTapBlock extends PageBlock
     {
         return Block::make('content-tip-tap')
             ->schema([
+                TextInput::make('toc')
+                    ->reactive()
+                    ->afterStateUpdated(function (Closure $set, $state) {
+                        /**
+                         * Generate the unique hash for the TOC item
+                         * and save to the tocHash field for link
+                         * generation while rendering
+                         */
+                        $set('tocHash', Str::slug($state) . '-' . uniqid());
+                    }),
+                Hidden::make('tocHash')
+                    ->disabled(),
                 TiptapEditor::make('content')
                     ->profile('simple')
                     ->required(),
